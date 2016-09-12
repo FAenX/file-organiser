0a690a147665735386f7fe946779e0e1a337c7ce
Emmanuel Davidson <faenxpert@gmail.com>
2016-09-12 11:55:01 +0300

    few changes

--- search.py
+++ search.py
@@ -12,78 +12,61 @@ import datetime
 import re
 import sys
 
-search_string=str(raw_input('enter search criteria eg "explicit" >> '))
-#size = int(raw_input('enter size of file eg "500 means > 500" >> '))
-location = os.path.join(os.getcwd(),'test_dir')
-_file=os.path.join(location,'file_list.txt')
+def search(file_list,search_string):
+	match=re.compile(search_string,re.I)	
+	for line in file_list:
+		filepath=line	
+		filename=os.path.split(line)
+		filename=filename[1]	
+		if match.search(line):	
+			print 'file name =>>',filename,' Title =>>',filepath
+			print '--------------------------------------------------------------'
 
-opened_file=open(_file,'r')
-_list = opened_file.readlines()
-_stripedList=[]
-_files=[]
+def search_by_last_modified(file_list,days):	
+	for line in file_list:
+		filepath=line	
+		filename=os.path.split(line)
+		filename=filename[1]
+		mstat=os.stat(line).st_ctime		
+		diff=datetime.datetime.today()-datetime.datetime.fromtimestamp(mstat)
+		diff=diff.days
+		if diff <=days: 
+			print 'last modified =>>',datetime.datetime.fromtimestamp(mstat),' Title =>>',filepath
+			print '--------------------------------------------------------------'
+		
 
-#print _list
-match=re.compile(search_string,re.I)
-
-for line in _list:
-	line=line.strip()
-	#print line
-	_stripedList.append(line)
-opened_file.close()
-
-
-def search():
-	
-
-	
-	for line in _stripedList:
+def search_by_size(file_list,size):
+	for line in file_list:
 		filepath=line	
 		filename=os.path.split(line)
 		filename=filename[1]
 		filesize=float(os.path.getsize(filepath))/1000000 
 		filesizeDisplay=str(filesize) + 'M'
-		if filesize > size and match.search(line):
-			_files.append(filepath)
-			
-			
-
-	
+		if filesize > size:	
 			print 'size =>>',filesizeDisplay,' Title =>>',filepath
 			print '--------------------------------------------------------------'
-	return _files
-
-def search_by_last_accesed():
 	
-	for line in _stripedList:
-		filepath=line	
-		filename=os.path.split(line)
-		filename=filename[1]
-		filesize=float(os.path.getsize(filepath))/1000000 
-		filesizeDisplay=str(filesize) + 'M'
-		lastaccesed=datetime.datetime.fromtimestamp(os.path.getctime(line))
-		
-			
-			
-
-	
-	return _files
-
-
-
-		
-	
-		
-
 def main():
-	#_search=search()
-	#
-	today=datetime.datetime.today()
-	_search_by_last_accesed=search_by_last_accesed()
-	for item in _search_by_last_accesed:
-		for (k,v) in item.items():
-			if today-v == datetime.timedelta(days=7):
-				print k ,' :  ', v
 
+	search_string='mp4'
+	location = os.path.join(os.getcwd(),'test_dir')
+	_file=os.path.join(location,'file_list.txt')
+
+	opened_file=open(_file,'r')
+	_list = opened_file.readlines()
+	main_List=[]
+	for line in _list:
+		line=line.strip()
+	
+		main_List.append(line)
+	opened_file.close()
+	
+	#search(main_List,search_string)
+	search_by_last_modified(main_List,10)
+
+	
+	
+	
 	
 		
 if __name__=='__main__':
