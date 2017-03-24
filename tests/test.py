fc4b8863adcbe438e62f0468502d26cb8bc16279
unknown <kips.emmanuel@gmail.com>
2017-03-24 19:22:15 +0300

    commit

--- tests/test.py
+++
@@ -1,4 +0,0 @@
-import files
-i=files.Files('/media/lopus/to be deleted')
-for f in i:
-     print(f)
--- unresolved/sha.py
+++
@@ -1,6 +0,0 @@
-import hashlib
-
-fhand = open('/home/lopus/new_coder/file-organiser/LISTS/FILES.LIST','r')
-data = fhand.read()
-fhand.close()
-checksum = hashlib.md5(data).hexdigest()
